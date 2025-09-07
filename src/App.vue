975832efc46ee428d5000cec5f4fe2be063a58ea
Sajjad Zare <sajadzarepur@gmail.com>
2025-09-07 20:18:10 +0330

    chore: config vue-shadcn to project

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,6 @@
-<script setup></script>
+<script setup>
+import Button from './components/ui/button/Button.vue'
+</script>
 
 <template>
   <h1 class="text-red-200">You did it!</h1>
@@ -6,6 +8,7 @@
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
+  <Button variant="outline"> Outline </Button>
 </template>
 
 <style scoped></style>
