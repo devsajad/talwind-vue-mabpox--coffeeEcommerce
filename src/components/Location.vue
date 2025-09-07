3ad1d47f7e9d4424b9ccd3c8c61bd7de1da3cd8d
Sajjad Zare <sajadzarepur@gmail.com>
2025-09-07 23:17:54 +0330

    feat: add Select component and related subcomponents with styles

--- src/components/Location.vue
+++ src/components/Location.vue
@@ -1,9 +1,28 @@
 <template>
   <div>
-    <p class="text-[#A2A2A2]">موقعیت مکانی</p>
+    <p class="text-[#A2A2A2] text-xs">موقعیت مکانی</p>
+    <Select dir="rtl" class="text-[#D8D8D8] outline-none">
+      <SelectTrigger class="w-[180px] border-none px-0 justify-start focus-visible:ring-0">
+        <SelectValue placeholder="مشهد ، صیادشیرازی" class="text-[#D8D8D8]" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectGroup>
+          <SelectItem value="firstAddress"> مشهد ، خیابان خیام </SelectItem>
+          <SelectItem value="secAddress"> مشهد ، صیادشیرازی </SelectItem>
+        </SelectGroup>
+      </SelectContent>
+    </Select>
   </div>
 </template>
+x
 
-<script setup></script>
+<script setup>
+import Select from './ui/select/Select.vue'
+import SelectContent from './ui/select/SelectContent.vue'
+import SelectGroup from './ui/select/SelectGroup.vue'
+import SelectItem from './ui/select/SelectItem.vue'
+import SelectTrigger from './ui/select/SelectTrigger.vue'
+import SelectValue from './ui/select/SelectValue.vue'
+</script>
 
 <style lang="scss" scoped></style>
