7ad777732204e2357af76ea24f58f62d6a16ce3d
Sajjad Zare <sajadzarepur@gmail.com>
2025-09-11 11:24:01 +0330

    feat: configure Supabase client with environment variables and add .env file for sensitive keys

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,4 @@
+import { SUPABASE_KEY, SUPABASE_URL } from '@/constants'
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://toaiqxopnbzacxndveet.supabase.co'
-const supabaseAnonKey =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRvYWlxeG9wbmJ6YWN4bmR2ZWV0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTcyNTgzNzgsImV4cCI6MjA3MjgzNDM3OH0._RfrS8HhRERzuPfqKcmNQvtZ_3Bu29i13ZhMTYCxcS4'
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
